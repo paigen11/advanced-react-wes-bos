ad5966b3639979c77ff236e47d33372ee293ca30
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2018-12-11 17:03:18 -0500

    hooking up prisma and making some graphql queries and mutations

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,25 @@
-const Mutations = {};
+const Mutations = {
+  createItem(parent, args, ctx, info) {
+    // todo: check if they are logged in
+
+    const item = ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info,
+    );
+
+    return item;
+  },
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   // create a dog
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // },
+};
 
 module.exports = Mutations;
