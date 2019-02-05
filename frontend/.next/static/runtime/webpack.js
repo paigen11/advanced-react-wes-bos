342dfc11c3026d22d87bc8982da89ed1560530fc
Paige Niedringhaus <paigen11@gmail.com>
2019-02-05 10:57:37 -0500

    adding sign up functionality on front end

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cd07c54444fd932e8aed";
+/******/ 	var hotCurrentHash = "dbd2bdf609b870f73f89";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
