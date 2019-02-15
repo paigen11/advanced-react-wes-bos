c05b99dfa7200eead28b01204854f18a7d11dd16
Paige Niedringhaus <paigen11@gmail.com>
2019-02-15 14:34:37 -0500

    adding user signin functionality

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -66,6 +66,27 @@ const Mutations = {
     // finally return user to browser
     return user;
   },
+  async signin(parent, { email, password }, ctx, info) {
+    // 1. check if there is a user with that email
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error(`No such user found for email ${email}`);
+    }
+    // 2. check if their password is correct
+    const valid = await bcyrpt.compare(password, user.password);
+    if (!valid) {
+      throw new Error(`Invalid password`);
+    }
+    // 3. generate the jwt token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // 4. set the cookie with the token
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365,
+    });
+    // 5. return the user
+    return user;
+  },
 };
 
 module.exports = Mutations;
