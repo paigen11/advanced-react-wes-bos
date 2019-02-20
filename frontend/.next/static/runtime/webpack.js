2f0004a9f1a603a6851845de795e69a297e720be
Paige Niedringhaus <paigen11@gmail.com>
2019-02-20 09:53:43 -0500

    adding signout

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f31ccb929348a58c6c12";
+/******/ 	var hotCurrentHash = "fe89ae521bff45a8ecd1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
